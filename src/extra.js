afc467c58eea0baa739845160c0a9b5efc954e3b
mezleca <mtksenpai@gmail.com>
2024-09-17 02:14:48 +0000

    test

--- src/extra.js
+++ src/extra.js
@@ -3,6 +3,8 @@ import { Message } from "discord.js";
 
 dotenv.config();
 
+const retarded_messages = ["soy professor de quimica", "el sergio ese corre metiendo noscopes xd jasjdasdjasds", "nossa y como mamas", "<@842353025904410634>"];
+
 /** @param {Message} interaction*/
 export const clean_channel = async (interaction) => {
     
@@ -15,13 +17,21 @@ export const clean_channel = async (interaction) => {
     }
     
     // create a new channel using the old_perms
-    await current_channel.guild.channels.create({
+    const new_channel = await current_channel.guild.channels.create({
         name: current_channel.name,
         type: current_channel.type,
-        parent: current_channel.parent,
-        permissionOverwrites: current_channel.permissionOverwrites.cache
+        permissionOverwrites: current_channel.permissionOverwrites.cache,
+        position: current_channel.position
     });
   
     // delete it :3
     await current_channel.delete("cleaned");
+    const random_msg = retarded_messages[Math.floor(Math.random() * retarded_messages.length - 1)];
+
+    if (!random_msg) {
+        console.log("invalid random message", random_msg);
+        return;
+    }
+    
+    await new_channel.send(random_msg);
 };
