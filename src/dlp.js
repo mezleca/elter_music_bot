00329f065033838b4a33e0163885cbc1be28b28d
mezleca <mtksenpai@gmail.com>
2024-09-20 22:43:01 -0300

    b

--- src/dlp.js
+++ src/dlp.js
@@ -58,6 +58,15 @@ const setup_dlp = () => {
 
         fs.writeFileSync(bin_path, bin_file.data);
 
+        // if you're on linux, im pretty sure you need to do this
+        if (process.platform = "linux") {
+            exec(`chmod +x ${bin_path}`, (err) => {
+                if (err) {
+                    throw Error("failed to give perm");
+                }
+            });
+        }
+
         console.log("[LOG] dlp setup completed");
         resolve();
     });
